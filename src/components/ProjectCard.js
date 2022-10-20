9bdc74ede9bd9f8756d361b5e937eae9d6d61371
Ayo Moses <ayomoses@afrimash.com>
2022-10-20 17:05:14 +0100

    updated target link

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <a  href={link} className="card-router">
+        <a  className="card-router" target="_blank" rel="noopener noreferrer" href={link} >
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
